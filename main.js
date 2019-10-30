6c075bce1bc6ee8abad1b71d604ba8844dc12a51
maaqoul <hicham.maaqoul@gmail.com>
2019-10-30 20:39:05 +0100

    add render functionality

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
-alert("hello");
+let myLibrary = [];
+
+function Book(title, author, numberOfPages) {
+  // the constructor...
+  this.title = title;
+  this.author = author;
+  this.numberOfPages = numberOfPages;
+}
+
+function addBookToLibrary() {
+  console.log("i've been clicked");
+  // do stuff here
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let numberOfPages = document.getElementById("numberOfPages").value;
+  const book = new Book(title, author, numberOfPages);
+  myLibrary.push(book);
+  title = author = numberOfPages = " ";
+
+  render();
+}
+
+const addButton = document.getElementById("add");
+addButton.addEventListener("click", () => {
+  addBookToLibrary();
+});
+
+function render() {
+  const tbody = document.getElementById("render");
+
+  for (const book of myLibrary) {
+    const tr = document.createElement("tr");
+    tbody.append(tr);
+    addColumn(tr, book.title);
+    console.log("book.title : ", book.title);
+    addColumn(tr, book.author);
+    console.log("book.author : ", book.author);
+    addColumn(tr, book.numberOfPages);
+    console.log("book.numberOfPages : ", book.numberOfPages);
+  }
+}
+
+function addColumn(tr, element) {
+  const td = document.createElement("td");
+  td.innerHTML = element;
+  tr.append(td);
+}
